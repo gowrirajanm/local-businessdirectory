5df42553e28003ea302e8f27e99f10e45cd6c78d
Gowrirajan. M <146209627+gowrirajanm@users.noreply.github.com>
2025-03-26 18:14:19 +0530

    Add files via upload

---
+++ local-business-directory/local-business-directory/src/test/java/com/business/local_business_directory/LocalBusinessDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.local_business_directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalBusinessDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
